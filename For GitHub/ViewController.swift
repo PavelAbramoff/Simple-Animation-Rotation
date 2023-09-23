4f724a7c603406103ccef28f6e5cd8b23445a3c6
Pavel.818 <Pavel.818@yandex.ru>
2023-09-22 22:59:19 -0700

    Initial Commit

---
+++ For GitHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  For GitHub
+//
+//  Created by Pavel on 2023-09-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
